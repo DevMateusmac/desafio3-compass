3374195c4dfc3fc4cec25bd39f63c70dd7879424
Mateus Maciulevičius <dev.mateusmac@gmail.com>
2024-01-26 17:28:54 -0300

    Feat: finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,48 @@
+import RootLayout from "./components/RootLayout";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+ 
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: '#FFFFFF',
+      },
+      secondary:{
+        main: '#FBA403'
+      }
+    },
+    components:{
+      MuiTextField:{
+        defaultProps:{
+          variant: 'outlined',
+        },
+        styleOverrides:{
+          root:{
+            '& .MuiOutlinedInput-notchedOutline':{
+              borderColor: '#FFFFFF',
+            },
+            '--TextField-brandBorderColor': 'red',
+            '--TextField-brandBorderHoverColor': '#FFFFFF',
+            '--TextField-brandBorderFocusedColor': '#FFFFFF',
+            '& label.Mui-focused': {
+              color: 'var(--TextField-brandBorderFocusedColor)',
+            },  
+          }
+
+        }
+      }
+    }
+  });
+
 
 function App() {
   return (
-    <>
-      
-    </>
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router}>
+        <RootLayout/>
+      </RouterProvider>
+    </ThemeProvider>
   )
 }
 
