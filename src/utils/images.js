a95f50d13362d05e4433ce34770c31554190fe66
Mateus Maciulevičius <dev.mateusmac@gmail.com>
2024-01-26 14:27:48 -0300

    Feat: creating dummy file with images to use on form (drive my own car)

---
+++ src/utils/images.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id: 1,  
+    imageURL: "/sedanOption.svg",
+    type: "Sedan"
+  },
+  {
+    id: 2,  
+    imageURL: "/suvVanOption.svg",
+    type: "SUV/Van"
+  },
+  {
+    id: 3,  
+    imageURL: "/semiLuxOption.svg",
+    type: "Semi Luxury"
+  },
+  {
+    id: 4,  
+    imageURL: "/luxuryOption.svg",
+    type: "Luxury car"
+  }
+]
