a971d0ebf8898d7bb720a88233b693106a1e1786
Mateus Maciulevičius <dev.mateusmac@gmail.com>
2024-01-26 17:25:51 -0300

    Feat: creating custom table

---
+++ src/components/table/CustomTablecell.jsx
@@ -0,0 +1,5 @@
+import { TableCell } from "@mui/material";
+// eslint-disable-next-line react/prop-types
+export default function CustomTablecell({children}){
+  return <TableCell sx={{color:"#FFFFFF"}}>{children}</TableCell>
+}
