1132bc2d454487ad25cea0203fdae8c8add5f2d0
Mateus Maciulevičius <dev.mateusmac@gmail.com>
2024-01-26 13:47:47 -0300

    Feat: creating file with images, description and title to add on CardItem

---
+++ src/utils/carClassData.js
@@ -0,0 +1,23 @@
+const carClassData = [
+  {
+    imageURL: "/car1sec2Image.svg",
+    title: "Basic",
+    description: "The best balance of price and comfort. You will not go wrong with our basic rides."
+  },
+  {
+    imageURL: "/car2sec2Image.svg",
+    title: "Comfort",
+    description: "If comfort is your priority, this is the ride for you. It's spacious and packed with features"
+  },
+  {
+    imageURL: "/car3sec2Image.svg",
+    title: "Business",
+    description: "Do you want to travel around the city in style? Make sure to select our business class rides."
+  },
+  {
+    imageURL: "/car4sec2Image.svg",
+    title: "Deluxe",
+    description: "The best ride for luxury and comfort. No compromises here. You'll surely get what you pay for."
+  }
+]
+export default carClassData
